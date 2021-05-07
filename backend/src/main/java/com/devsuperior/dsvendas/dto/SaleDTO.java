140b502859e9aefeb47e1131a0ac201f43d16126
Livia <livinha.guimaraes.013@gmail.com>
2021-05-07 19:08:57 -0300

    Group by search

--- backend/src/main/java/com/devsuperior/dsvendas/dto/SaleDTO.java
+++ backend/src/main/java/com/devsuperior/dsvendas/dto/SaleDTO.java
@@ -1,11 +1,14 @@
 package com.devsuperior.dsvendas.dto;
 
+import java.io.Serializable;
 import java.time.LocalDate;
 
 import com.devsuperior.dsvendas.entities.Sale;
 
-public class SaleDTO {
+public class SaleDTO implements Serializable {
 		
+	private static final long serialVersionUID = 1L;
+	
 	private Long id;
 	private Integer visited;
 	private Integer deals;
